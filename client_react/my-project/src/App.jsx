6679f0f96e6b45c5f4d19fcb2e37c37a163ab6bc
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2025-02-22 07:39:25 +0900

    [Feat] 작업 시작

---
+++ client_react/my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/layout/header";
+import Main from "./components/layout/main";
+import Footer from "./components/layout/footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
