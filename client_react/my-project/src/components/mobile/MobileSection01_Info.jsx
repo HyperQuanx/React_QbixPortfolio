f29edae034b06dd898cf18279e533321d293737e
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2025-05-08 07:12:00 +0900

    [Feat] Info 1차 publishing

--- client_react/my-project/src/components/mobile/MobileSection01_Info.jsx
+++ client_react/my-project/src/components/mobile/MobileSection01_Info.jsx
@@ -19,6 +19,7 @@ import {
 
 const MobileSection01_Info = () => {
   return (
+    // ㄴㄻㄹ
     <section>
       <SectionTitle>Info</SectionTitle>
       <SectionTitleUnderLine></SectionTitleUnderLine>
