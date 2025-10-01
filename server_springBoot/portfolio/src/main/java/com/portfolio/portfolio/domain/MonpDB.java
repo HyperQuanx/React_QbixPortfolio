52c74bfab7ab606cafcc0d6a6b9ee7d2d5af5ef3
Qbix <qbixroqkfwk@gmail.com>
2025-10-02 03:16:41 +0900

    [Fix] import 구문 빼먹어서 CI/CD 오류남

--- server_springBoot/portfolio/src/main/java/com/portfolio/portfolio/domain/MonpDB.java
+++ server_springBoot/portfolio/src/main/java/com/portfolio/portfolio/domain/MonpDB.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter
