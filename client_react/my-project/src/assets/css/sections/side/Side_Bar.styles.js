c622cf4a42c1cb15727eefe6bbc815574f64a097
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2025-10-16 16:54:02 +0900

    [Fix] 임시로 사이드바 가리기

--- client_react/my-project/src/assets/css/sections/side/Side_Bar.styles.js
+++ client_react/my-project/src/assets/css/sections/side/Side_Bar.styles.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Side_Bar_Container = styled.div`
-  z-index: 10000;
+  z-index: 100;
   position: relative;
   left: 7vw;
   top: 7vh;
   width: 100px;
   height: 100px;
-  background-color: #000;
+  /* background-color: #000; */
 `;
