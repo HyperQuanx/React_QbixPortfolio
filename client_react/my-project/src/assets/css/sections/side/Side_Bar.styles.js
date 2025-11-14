ecb14ba732be1e2075b435637eb710ebaa769b3c
Qbix <qbixroqkfwk@gmail.com>
2025-11-14 09:28:47 +0900

    [Fix] SEO 신경쓰기

--- client_react/my-project/src/assets/css/sections/side/Side_Bar.styles.js
+++ client_react/my-project/src/assets/css/sections/side/Side_Bar.styles.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const Side_Bar_Container = styled.div`
   z-index: 10000;
   position: relative;
-  left: 7vw;
-  top: 7vh;
-  width: 100px;
-  height: 100px;
-  background-color: #000;
+  left: 5vw;
+  top: 5vh;
+  width: 10vw;
+  height: 30vh;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
 `;
